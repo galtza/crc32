5cea6b18faeba6d0acde4b8883ea1a4b97aaa1ee
QCSTUDIO\34629 <igaltza@gmail.com>
2022-11-22 21:06:58 +0100

    Added crc-32-c polynomial support on top of the crc-32. Also, simplified the interface (remove redundant 'checksum_from_literal')

--- main.cpp
+++ main.cpp
@@ -3,44 +3,35 @@
 #include <cassert>
 #include "crc32.h"
 
-int main () {
-
+int main() {
     using namespace std;
 
-    // Simple test
-    constexpr auto short_ct = qcstudio::crc32::from_literal("Hello world");
-    const     auto short_rt = qcstudio::crc32::from_string("Hello world");
-    assert(short_ct == short_rt);
+#undef TEST_STRING
+#define TEST_STRING "Hello world"
 
-    cout << hex << (uint32_t)short_ct << "\n";
-    cout << hex << (uint32_t)short_rt << "\n";
+    const auto     short_cstring                    = TEST_STRING;
+    constexpr auto ref_result_from_literal_crc_32   = (uint32_t)qcstudio::crc32::from_literal(TEST_STRING);
+    constexpr auto ref_result_from_literal_crc_32_c = (uint32_t)qcstudio::crc32::from_literal(TEST_STRING, qcstudio::crc32::crc_32_c_poly);
+    const auto     result_from_string_crc_32        = (uint32_t)qcstudio::crc32::from_string(short_cstring);
+    const auto     result_from_string_crc_32_c      = (uint32_t)qcstudio::crc32::from_string(short_cstring, qcstudio::crc32::crc_32_c_poly);
+    const auto     result_from_buffer_crc_32        = (uint32_t)qcstudio::crc32::from_buffer((uint8_t*)short_cstring, strlen(short_cstring));
+    const auto     result_from_buffer_crc_32_c      = (uint32_t)qcstudio::crc32::from_buffer((uint8_t*)short_cstring, strlen(short_cstring), qcstudio::crc32::crc_32_c_poly);
+
+    assert(result_from_string_crc_32 == ref_result_from_literal_crc_32);
+    assert(result_from_buffer_crc_32 == ref_result_from_literal_crc_32);
+    assert(result_from_string_crc_32_c == ref_result_from_literal_crc_32_c);
+    assert(result_from_buffer_crc_32_c == ref_result_from_literal_crc_32_c);
+
+    cout << "\"" << TEST_STRING << "\"\n";
     cout << endl;
-
-    // Long test
-    constexpr auto long_ct = qcstudio::crc32::from_literal(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-        "Pellentesque iaculis neque sem, eget congue justo euismod"
-        " nec. Mauris ornare, sem sit amet ullamcorper vestibulum,"
-        " urna arcu tristique massa, sit amet imperdiet velit dolor"
-        " vel felis. Aliquam non arcu aliquam, mattis eros non,"
-        " porttitor lectus. Maecenas sed augue a dui gravida interdum."
-        " Cras quis risus est. Donec sed tincidunt lectus, at tempor "
-        "tortor. Duis ut convallis purus. Nam mattis felis eros, quis "
-        "fermentum enim vulputate in."
-    );
-    const auto long_rt = qcstudio::crc32::from_string(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-        "Pellentesque iaculis neque sem, eget congue justo euismod"
-        " nec. Mauris ornare, sem sit amet ullamcorper vestibulum,"
-        " urna arcu tristique massa, sit amet imperdiet velit dolor"
-        " vel felis. Aliquam non arcu aliquam, mattis eros non,"
-        " porttitor lectus. Maecenas sed augue a dui gravida interdum."
-        " Cras quis risus est. Donec sed tincidunt lectus, at tempor "
-        "tortor. Duis ut convallis purus. Nam mattis felis eros, quis "
-        "fermentum enim vulputate in."
-    );
-    assert(long_ct == long_rt);
-    cout << hex << (uint32_t)long_ct << "\n";
-    cout << hex << (uint32_t)long_rt << "\n";
+    cout << "crc-32 polynomial\n";
+    cout << "0x" << hex << (uint32_t)ref_result_from_literal_crc_32 << "(from_literal)\n";
+    cout << "0x" << hex << (uint32_t)result_from_string_crc_32 << "(from_string)\n";
+    cout << "0x" << hex << (uint32_t)result_from_buffer_crc_32 << "(from_buffer)\n";
+    cout << endl;
+    cout << "crc-32-c polynomial\n";
+    cout << "0x" << hex << (uint32_t)ref_result_from_literal_crc_32_c << "(from_literal)\n";
+    cout << "0x" << hex << (uint32_t)result_from_string_crc_32_c << "(from_string)\n";
+    cout << "0x" << hex << (uint32_t)result_from_buffer_crc_32_c << "(from_buffer)\n";
     cout << endl;
 }
